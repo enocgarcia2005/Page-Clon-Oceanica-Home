4d2872245f552b67faa5523706264bf11dba8b80
enocnehemiasgarciaalcantara <enocgarcia2005@gmail.com>
2023-10-25 14:25:29 -0600

    update bullet

--- js/scripts.js
+++ js/scripts.js
@@ -1,62 +1,63 @@
 const btnLeft = document.querySelector(".btn-left"),
-      btnRight = document.querySelector(".btn-right")
-      carouselContainer = document.querySelector("#carousel-container"),
-      carouselItems = document.querySelectorAll(".carousel-item"),
-      bullets = document.querySelectorAll(".bullet");
+    btnRight = document.querySelector(".btn-right");
+(carouselContainer = document.querySelector("#carousel-container")),
+    (carouselItems = document.querySelectorAll(".carousel-item")),
+    (bullets = document.querySelectorAll(".bullet-item"));
 
-      btnLeft.addEventListener("click", e => moveToLeft())
-      btnRight.addEventListener("click", e => moveToRight())
-      bullets.forEach((element,i) => {
-        bullets[i].addEventListener("click", e => configBullet(i))
-      });
+btnLeft.addEventListener("click", (e) => moveToLeft());
+btnRight.addEventListener("click", (e) => moveToRight());
+bullets.forEach((element, i) => {
+    bullets[i].addEventListener("click", (e) => configBullet(i));
+});
 
-      let operation = 0
-      let widthImg = 100 / carouselItems.length
-      let count = 0
+let operation = 0;
+let widthImg = 100 / carouselItems.length;
+let count = 0;
 
-      function configBullet(position){
-        count = position
-        operation = count * widthImg
-        animateCarousel(operation)
-        paintBullet(position)
-      }
+function configBullet(position) {
+    count = position;
+    operation = count * widthImg;
+    animateCarousel(operation);
+    paintBullet(position);
+    console.log(operation)
+}
 
-      function moveToRight(){
-        count++
-        if(count <= carouselItems.length - 1){
-            operation += widthImg
-            animateCarousel(operation)
-            paintBullet(count)
-            return;
-        }
-        count = 0
-        operation = 0
-        animateCarousel(operation)
-        paintBullet(count)
-      }
+function moveToRight() {
+    count++;
+    if (count <= carouselItems.length - 1) {
+        operation += widthImg;
+        animateCarousel(operation);
+        paintBullet(count);
+        return;
+    }
+    count = 0;
+    operation = 0;
+    animateCarousel(operation);
+    paintBullet(count);
+}
 
-      function moveToLeft(){
-        count--
-        if(count >= 0){
-            operation -= widthImg
-            animateCarousel(operation)
-            paintBullet(count)
-            return;
-        }
-        count = carouselItems.length - 1
-        operation = widthImg * count
-        paintBullet(count)
-        animateCarousel(operation)
-      }
+function moveToLeft() {
+    count--;
+    if (count >= 0) {
+        operation -= widthImg;
+        animateCarousel(operation);
+        paintBullet(count);
+        return;
+    }
+    count = carouselItems.length - 1;
+    operation = widthImg * count;
+    paintBullet(count);
+    animateCarousel(operation);
+}
 
-      function animateCarousel(operation){
-        carouselContainer.style.transform = `translate(-${operation}%)`
-        carouselContainer.style.transition = "all ease .6s"
-      }
+function animateCarousel(operation) {
+    carouselContainer.style.transform = `translateX(-${operation}%)`;
+    carouselContainer.style.transition = "all ease .6s";
+}
 
-      function paintBullet(position){
-        bullets.forEach((element,i) =>{
-          bullets[i].classList.remove('actived')
-        })
-        bullets[position].classList.add('actived')
-      }
+function paintBullet(position) {
+    bullets.forEach((element, i) => {
+        bullets[i].classList.remove("actived");
+    });
+    bullets[position].classList.add("actived");
+}
